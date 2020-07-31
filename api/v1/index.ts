1291f8cae2c3dad03820e4333df79736cccc9eb4
zafar26 <qutubuddinzafar2616118@gmail.com>
2020-07-31 20:31:57 +0530

    Added Confirmation page in mjml

--- api/v1/index.ts
+++ api/v1/index.ts
@@ -1,6 +1,9 @@
 import { NowRequest, NowResponse } from "@vercel/node";
+import { Myhtml } from "../../utils/mjml";
 
 export default function (req: NowRequest, res: NowResponse) {
+  // console.log(Myhtml.errors,"MYHTML___ERRORS")
+  // res.send(Myhtml.html);
   const { name = "World" } = req.query;
   res.send(`Hello ${name}`);
 }
